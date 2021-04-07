13c663e30e54136563f571652512e043a17bf22a
Ahmed tarek <atarek@visions-tech.net>
2021-04-07 04:28:59 +0200

    first commit

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $('.offers-owl-carousel').owlCarousel({
+        margin: 20,
+        stagePadding: 20,
+        nav: false,
+        loop: true,
+        dots: true,
+        items: 1
+    })
+
+    $('.categories-owl-carousel').owlCarousel({
+        margin: 20,
+        stagePadding: 20,
+        nav: false,
+        loop: true,
+        dots: true,
+        items: 4
+    })
+
+})
